6019ff1fc2c22270fb8b8e0e8d9596bb50d48083
Scott Parks <swparks040@gmail.com>
2022-09-01 13:05:17 -0500

    added ticket search, parent container for tickets and list

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -4,7 +4,7 @@ import "./Tickets.css"
 //import useNavigate from react router DOM
 import { useNavigate } from "react-router-dom"
 
-export const TicketList = () => {
+export const TicketList = ({ searchTermState }) => {
     const [tickets, setTickets] = useState([])
     const [filteredTickets, setFiltered] = useState([])
     const [emergency, setEmergency] = useState(false)
@@ -13,6 +13,17 @@ export const TicketList = () => {
     const localHoneyUser = localStorage.getItem("honey_user")
     const honeyUserObject = JSON.parse(localHoneyUser)
     // Almost always going to be for initial state in useEffect()
+
+useEffect(
+    () => {
+        const searchedTickets = tickets.filter(ticket => {
+            return ticket.description.toLowerCase().startsWith(searchTermState.toLowerCase())
+        })
+        setFiltered(searchedTickets)
+    }, 
+    [ searchTermState ]
+)
+
     useEffect(
         () => {
             if (emergency) {
---
+++ src/components/tickets/TicketSearch.js
@@ -0,0 +1,14 @@
+//name of the key goes into the props position 
+export const TicketSearch = ({setterFunction}) => {
+    return (
+        <div>
+        <input 
+            onChange={
+                (changeEvent) => {
+                    setterFunction(changeEvent.target.value)
+                }
+            }
+        type="text" placeholder="Enter search terms" />
+        </div>
+    )
+}
