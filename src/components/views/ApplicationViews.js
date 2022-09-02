ef4c1b411c5bfc4fe1a6903c32e00e837146768e
Scott Parks <swparks040@gmail.com>
2022-09-01 20:21:19 -0500

    add summo schtuff, Honey Rae Chap. 11 complete

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,25 +1,18 @@
-import { Outlet, Route, Routes } from "react-router-dom"
-import { TicketContainer } from "../tickets/TicketContainer"
-import { TicketForm } from "../tickets/TicketForm"
-// import { TicketList } from "../tickets/TicketList"
-// import { TicketSearch } from "../tickets/TicketSearch"
+import { CustomerViews } from "./CustomerViews"
+import { EmployeeViews } from "./EmployeeViews"
+
 
 export const ApplicationViews = () => {
-	return (
-        <Routes>
-            <Route path="/" element={
-                <>
-                    <h1>Honey Rae Repair Shop</h1>
-                    <div>Your one-stop-shop to get all your electronics fixed</div>
+	
+	const localHoneyUser = localStorage.getItem("honey_user")
+    const honeyUserObject = JSON.parse(localHoneyUser)
+	
+	if(honeyUserObject.staff) {
+		//Return employee views
+		return <EmployeeViews />
+	}
+	else {
 
-                    <Outlet />
-                </>
-            }>
-
-                <Route path="tickets" element={ <TicketContainer />} />
-
-				<Route path="ticket/create" element={ <TicketForm /> } />
-            </Route>
-        </Routes>
-    )
+		return <CustomerViews />
+	}
 }
